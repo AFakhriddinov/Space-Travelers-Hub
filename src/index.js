3921576a071176615cc95124d89faab91b874e89
Ashnaali3255 <ashnaalipk@gmail.com>
2023-06-01 14:42:45 +0500

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
