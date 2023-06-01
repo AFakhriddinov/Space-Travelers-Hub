02c8768770725d1e7d547fbb0f9c47ae2df9d41a
Ashnaali3255 <ashnaalipk@gmail.com>
2023-06-01 14:52:48 +0500

    delete files

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -1,43 +1,14 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchMissions } from '../redux/mission/missionSlice';
-import './styles/Missions.css';
-import Mission from './mission';
-import Loader from './Loader';
-import 'bootstrap/dist/css/bootstrap.css';
 
 const Missions = () => {
   const dispatch = useDispatch();
-  const { missions, isLoading } = useSelector((store) => store.missions);
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
-  if (isLoading) {
-    return <Loader />;
-  }
   return (
-    <div className="container-fluid px-5">
-      <table className="table table-hover table-striped table-responsive">
-        <thead>
-          <tr>
-            <th className="heading" scope="col">Missions</th>
-            <th className="heading" scope="col">Description</th>
-            <th className="heading" scope="col">Status</th>
-            <th className="heading" scope="col">{' '}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {missions.map((mission) => (
-            <Mission
-              name={mission.name}
-              key={mission.id}
-              id={mission.id}
-              description={mission.description}
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <div>Missions</div>
   );
 };
 
