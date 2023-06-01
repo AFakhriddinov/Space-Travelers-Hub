2b275fc9a652935e8e1587c148aa15745d4eaa91
Ashnaali3255 <ashnaalipk@gmail.com>
2023-06-01 17:24:22 +0500

    Add funtion to Join Mission

--- src/Components/Mission.js
+++ src/Components/Mission.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../redux/mission/missionSlice';
 
 const Mission = ({
-  name, description,
-}) => (
-  <tr>
-    <th className="heading" scope="row">{name}</th>
-    <td className="heading col-lg-7">{description}</td>
-    <td className="btns">
-      <span className="badge text-bg-secondary">Not A Member</span>
-    </td>
-    <td className="btns">
-      <button
-        type="button"
-        className="btn btn-outline-secondary test-small btn-sm"
-      >
-        Join Mission
-      </button>
-    </td>
-  </tr>
-);
+  id, name, description, reserved,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <tr>
+      <th className="heading" scope="row">{name}</th>
+      <td className="heading col-lg-7">{description}</td>
+      <td className="btns">
+        <span className="badge text-bg-secondary">Not A Member</span>
+      </td>
+      <td className="btns">
+        {reserved ? (
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm"
+          >
+            Leave Mission
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-outline-secondary test-small btn-sm"
+            onClick={() => dispatch(joinMission(id))}
+          >
+            Join Mission
+          </button>
+        )}
+      </td>
+    </tr>
+  );
+};
 
 Mission.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default Mission;
