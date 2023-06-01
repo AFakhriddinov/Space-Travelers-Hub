d7cc65bae5d5d88836014847ba2e7f7d96042d60
Abdulaziz Fakhriddinov <mr_abdulaziz777@list.ru>
2023-06-01 08:40:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Missions from './Components/Missions';
